05c0104a22544fe371508b8bc71213d53b5372b4
nataraj.ramamoorthy <nataraj.ramamoorthy13@gmail.com>
2024-06-22 13:15:11 +0530

    first-commit

---
+++ account-impersonation/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    google = {
+      version = "~> 4.7.0"
+    }
+  }
+}
+
+terraform {
+
+  backend "gcs" {
+    bucket = "<bucket-name>"
+    prefix = "<prefix>"
+
+    impersonate_service_account = "<service-account-email>"
+
+  }
+
+}
